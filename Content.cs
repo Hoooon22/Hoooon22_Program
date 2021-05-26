d996ad7ddaf829c68644b88b07a4fad75ce5d436
Hoooon22 <momo990305@gmail.com>
2021-05-26 16:33:49 +0900

    Content.cs

--- Content.cs
+++ Content.cs
@@ -9,5 +9,12 @@ namespace Hoooon22_Program
 {
     class Content
     {
+        [PrimaryKey,AutoIncrement]
+        public int Id { get; set; }
+        public string Date { get; set; }
+        public string Title { get; set; }
+        public string Amount { get; set; }
+        public string Source { get; set; }
+        public string Remarks { get; set; }
     }
 }
