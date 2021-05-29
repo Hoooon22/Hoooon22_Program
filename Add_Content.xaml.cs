c7cb6a68e0cd7ed951daba2990420ea4f958d955
Hoooon22 <momo990305@gmail.com>
2021-05-29 15:26:17 +0900

    DB Connection Code

--- Add_Content.xaml.cs
+++ Add_Content.xaml.cs
@@ -11,7 +11,10 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
-using MySql.
+using System.Data;
+using MySql.Data;
+using MySql.Data.Common;
+using MySql.Data.MySqlClient;
 
 namespace Hoooon22_Program
 {
@@ -21,6 +24,21 @@ namespace Hoooon22_Program
     public partial class Add_Content : Window
     {
         MySqlConnection conn;
+        string dbTable = "finace_content_tb"; // 접근할 테이블 설정
+        string dbInfo()
+        {
+            string dbServer = "";
+            string dbDatabase = "";
+            string dbUid = "";
+            string dbPwd = "";
+            string dbSslMode = "none";
+            string Conn = "Server=" + dbServer + ";"
+                        + "Database=" + dbDatabase + ";"
+                        + "Uid=" + dbUid + ";"
+                        + "Pwd=" + dbPwd + ";"
+                        + "SslMode=" + dbSslMode;
+            return Conn;
+        }
 
         public Add_Content()
         {
@@ -29,7 +47,21 @@ namespace Hoooon22_Program
 
         private void Submit_Click(object sender, RoutedEventArgs e)
         {
+            try
+            {
+                using (conn = new MySqlConnection(dbInfo()))
+                {
+                    conn.Open();
+                    if (conn.State == ConnectionState.Open) // System.Data
+                    {
+                        MessageBox.Show("서버에 연결");
+                    }
 
+                    // Mysql DB Table 생성
+
+                }
+            }
+            catch (Exception ex) { MessageBox.Show(ex.Message); }
         }
     }
 }
