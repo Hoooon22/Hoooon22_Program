6c3eca4ff81d067a747b73671e8d2dd587da1e0d
Hoooon22 <momo990305@gmail.com>
2021-05-30 20:09:51 +0900

    Finance.cs DB - 01

--- Finance.xaml.cs
+++ Finance.xaml.cs
@@ -11,6 +11,10 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using System.Data;
+using MySql.Data;
+using MySql.Data.Common;
+using MySql.Data.MySqlClient;
 
 namespace Hoooon22_Program
 {
@@ -19,6 +23,25 @@ namespace Hoooon22_Program
     /// </summary>
     public partial class Finance : Window
     {
+        // DB Load
+
+        MySqlConnection conn;
+        string dbTable = "finace_content_tb"; // 접근할 테이블 설정
+        string dbInfo()
+        {
+            string dbServer = "localhost";
+            string dbDatabase = "hpdb";
+            string dbUid = "root";
+            string dbPwd = "password";
+            string dbSslMode = "none";
+            string Conn = "Server=" + dbServer + ";"
+                        + "Database=" + dbDatabase + ";"
+                        + "Uid=" + dbUid + ";"
+                        + "Pwd=" + dbPwd + ";"
+                        + "SslMode=" + dbSslMode;
+            return Conn;
+        }
+
         ScaleTransform scale = new ScaleTransform();
         double originWidth, originHeight;
 
@@ -41,6 +64,25 @@ namespace Hoooon22_Program
             }
 
             this.SizeChanged += new SizeChangedEventHandler(Window_Changed);
+
+            // DB Laod
+            try
+            {
+                using (conn = new MySqlConnection(dbInfo()))
+                {
+                    conn.Open();
+                    if (conn.State == ConnectionState.Open) // System.Data
+                    {
+                        MessageBox.Show("서버에 연결");
+                    }
+
+
+
+                    conn.Close(); // 연결 종료
+                    Close(); // 윈도우 종료
+                }
+            }
+            catch (Exception ex) { MessageBox.Show(ex.Message); }
         }
 
         private void ChangeSize(double width, double height)
